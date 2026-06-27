7b95178a5054f70873110f4504826d83c8521b1d
Vinicius Silva <105992683+vinisilva910@users.noreply.github.com>
2026-06-27 16:53:30 -0300

    Inserção do Código test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,1 +1,29 @@
+from src.tasks import TaskManager
 
+
+def test_criar_tarefa():
+    manager = TaskManager()
+    manager.criar_tarefa("Estudar Python")
+
+    assert len(manager.tasks) == 1
+    assert manager.tasks[0].titulo == "Estudar Python"
+
+
+def test_concluir_tarefa():
+    manager = TaskManager()
+    manager.criar_tarefa("Estudar")
+
+    manager.concluir_tarefa(1)
+
+    assert manager.tasks[0].concluida is True
+
+
+def test_remover_tarefa():
+    manager = TaskManager()
+
+    manager.criar_tarefa("A")
+    manager.criar_tarefa("B")
+
+    manager.remover_tarefa(1)
+
+    assert len(manager.tasks) == 1
